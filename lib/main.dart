7828ac021c6548e70c9280b25cdf7183a5087547
Divyanshu Prince <divyanshuprince764@gmail.com>
2022-08-07 22:21:29 +0530

    I am poor app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+// import 'package:AndroidStudioProjects/Images';
+// import 'package:i_am_poor/';
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I Am Poor!!'),
+          backgroundColor: Colors.black12,
+        ),
+        body: Center(
+          child: Image(  image: AssetImage('Images/index.png'),
+            // NetworkImage('https://picsum.photos/200'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
